8f6db6392214ed42afbfd792213c570ef29d29c2
kwankunghkg <92127036+kwankunghkg@users.noreply.github.com>
2025-07-24 14:43:01 +0800

    Update counter.v

--- IC/Verilog/EDAplayground/counter.v
+++ IC/Verilog/EDAplayground/counter.v
@@ -9,12 +9,12 @@ reg [WIDTH-1 : 0] out;
 wire clk, reset;
 
 always @(posedge clk)
-out <= out + 1;
+  out <= out + 1;
 
 always @reset
-if (reset)
-assign out = 0;
-else
-deassign out;
+  if (reset)
+    assign out = 0;
+  else
+    deassign out;
 
 endmodule // counter
